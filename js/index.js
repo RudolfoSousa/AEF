bdce705422f6a7fb846d43b2f315552bba4977db
rudolfo.sousa <rudolfo.sousa@agenciakindle.com.br>
2019-04-26 09:52:06 -0300

    Menu deslizante

--- js/index.js
+++ js/index.js
@@ -1,3 +1,15 @@
-$(document).ready(function(){
-	
-})
+$(document).ready(function() {
+  menuScroll();
+});
+
+function menuScroll() {
+	$(document).on('scroll', function(){
+		var scrollTop = $(window).scrollTop();
+		console.log(scrollTop);
+		if (scrollTop >= 100) {
+			$('header').addClass('active');
+		}else{
+			$('header').removeClass('active')
+		}
+	})
+}
